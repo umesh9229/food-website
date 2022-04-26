bbc91bbb3a7509aab9f6762c54e18ae494e824cd
Umesh patel <100286708+umesh6342@users.noreply.github.com>
2022-04-26 13:23:10 +0530

    frist commit

---
+++ src/components/common/Logo/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.png';
+import './styles.css';
+
+const Logo = () => (
+  <div className='header-content_logo'>
+    <div>
+      <img src={logo} alt='logo' />
+      <span>
+        <b>Food's Restaurent</b>
+      </span>
+    </div>
+    <p>
+     
+    </p>
+  </div>
+);
+
+export default Logo;
